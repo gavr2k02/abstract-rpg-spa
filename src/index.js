a0837fd32e2a3361eb1dea456b12b32aa10f1b73
gavr2k02 <gavrilin2k02@gmail.com>
2021-04-09 18:48:16 +0300

    hide window_redux

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import reportWebVitals from "./reportWebVitals";
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
+        // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        //     window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
